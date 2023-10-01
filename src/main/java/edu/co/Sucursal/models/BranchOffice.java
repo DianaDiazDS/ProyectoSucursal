2cad8135a7f1db723964a7c0c4e3c984fb7827e5
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-10-01 14:53:50 -0500

    Arreglo de loan

--- src/main/java/edu/co/Sucursal/models/BranchOffice.java
+++ src/main/java/edu/co/Sucursal/models/BranchOffice.java
@@ -1,7 +1,80 @@
 package edu.co.Sucursal.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table (name = "BRANCHOFFICE")
 public class BranchOffice {
 
+    @Id
+    @Column(name = "ID_BRANCH_OFFICE")
+    private Long idBranchOffice;
 
+    @Column(name = "NAME")
+    private String name;
 
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @OneToMany(mappedBy = "BRANCHOFFICE", orphanRemoval = true, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonManagedReference
+    @JsonIgnoreProperties("tags")
+    private List<Loan> loans;
+
+    public Long getIdBranchOffice() {
+        return idBranchOffice;
+    }
+
+    public void setIdBranchOffice(Long idBranchOffice) {
+        this.idBranchOffice = idBranchOffice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Loan> getLoans() {
+        return loans;
+    }
+
+    public void setLoans(List<Loan> loans) {
+        this.loans = loans;
+    }
 }
--- src/main/java/edu/co/Sucursal/models/Loan.java
+++ src/main/java/edu/co/Sucursal/models/Loan.java
@@ -1,36 +1,38 @@
 package edu.co.Sucursal.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import javax.persistence.*;
-import java.util.List;
-import edu.co.Sucursal.models.User;
 
 @Entity
-@Table(name = "Loan")
+@Table(name = "LOAN")
 public class Loan {
+
     @Id
-    @Column(name = "id_loan")
+    @Column(name = "ID_LOAN")
     private Long idLoan;
-    @Column(name = "report_type")
+    @Column(name = "REPORT_TYPE")
     private String reportType;
-    @Column(name = "amount")
+    @Column(name = "AMOUNT")
     private int amount;
-    @Column(name = "term")
+    @Column(name = "TERM")
     private String term;
-    @Column(name = "interest_rate")
+    @Column(name = "INTEREST_RATE")
     private double insterestRate;
-    @Column(name = "loan_type")
+    @Column(name = "LOAN_TYPE")
     private String loanType;
-    @Column(name = "last_payment_date")
+    @Column(name = "LAST_PAYMENT_DATE")
     private String lastPaymentDate;
-    @Column(name = "state")
+    @Column(name = "STATE")
     private String state;
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonBackReference
-    @JoinColumn(name = "id_user" )
+    @JoinColumn(name = "USER" )
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name = "BRANCH_OFFICE" )
+    private BranchOffice branchOffice;
 
     public Loan() {
 
@@ -40,5 +42,85 @@ public class Loan {
     public String toString() {
         // TODO Auto-generated method stub
         return super.toString();
+    }
+
+    public Long getIdLoan() {
+        return idLoan;
+    }
+
+    public void setIdLoan(Long idLoan) {
+        this.idLoan = idLoan;
+    }
+
+    public String getReportType() {
+        return reportType;
+    }
+
+    public void setReportType(String reportType) {
+        this.reportType = reportType;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public double getInsterestRate() {
+        return insterestRate;
+    }
+
+    public void setInsterestRate(double insterestRate) {
+        this.insterestRate = insterestRate;
+    }
+
+    public String getLoanType() {
+        return loanType;
+    }
+
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+    public String getLastPaymentDate() {
+        return lastPaymentDate;
+    }
+
+    public void setLastPaymentDate(String lastPaymentDate) {
+        this.lastPaymentDate = lastPaymentDate;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public BranchOffice getBranchOffice() {
+        return branchOffice;
+    }
+
+    public void setBranchOffice(BranchOffice branchOffice) {
+        this.branchOffice = branchOffice;
     }
 }
